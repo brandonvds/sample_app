da772e608ae46bcef41b29dcc1ba75bdb1654fd6
brandon <brandonvds@live.nl>
2012-06-01 12:52:38 +0000

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Brandon v/d Sluis"
+    email    "brandonvds@live.nl"
+    password "isabella"
+    password_confirmation "isabella"
+  end
+end
